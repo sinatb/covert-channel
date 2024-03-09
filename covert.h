82afe31e6927ea8fc2c6e4721e74b9aafe6b9fe1
sinatb <sinatb.dev@gmail.com>
2024-03-09 16:00:38 +0330

    feat : made message into chunks and then sent it

--- covert.h
+++ covert.h
@@ -11,7 +11,7 @@
 #include <stdexcept>
 class covert_handler{
 public:
-    covert_handler(std::string& ip) : ip(ip)
+    explicit covert_handler(std::string& ip) : ip(ip)
     {
         ipaddr = inet_addr(ip.c_str());
         if (ipaddr == INADDR_NONE) {
@@ -22,51 +22,51 @@ public:
             throw std::runtime_error("Failed to create ICMP file handle");
         }
     }
-    char* send_covert_message(){
-        char* ret;
-        ReplySize = sizeof(ICMP_ECHO_REPLY) + sizeof(SendData);
-        ReplyBuffer = (VOID*) malloc(ReplySize);
-        if (ReplyBuffer == NULL) {
-            throw std::runtime_error("Failed to allocate memory for reply buffer");
-        }
-        dwRetVal = IcmpSendEcho(hIcmpFile,
-                                ipaddr,
-                                SendData,
-                                sizeof(SendData),
-                                NULL,
-                                ReplyBuffer,
-                                ReplySize,
-                                1000);
-        if (dwRetVal != 0) {
-            PICMP_ECHO_REPLY pEchoReply = (PICMP_ECHO_REPLY)ReplyBuffer;
-            struct in_addr ReplyAddr;
-            ReplyAddr.S_un.S_addr = pEchoReply->Address;
-            std::cout <<"\tSent icmp message to %s\n" << ip << '\n';
-            if (dwRetVal > 1) {
-                std::cout << "\tReceived %ld icmp message responses\n" << dwRetVal <<'\n';
-                std::cout << "\tInformation from the first response:\n" << '\n';
+    ~covert_handler(){
+        IcmpCloseHandle(hIcmpFile);
+    }
+    char* send_covert_message(char* message){
+        size_t chunk_size = 32;
+        size_t message_size = strlen(message);
+        for (size_t offset = 0 ; offset < message_size ; offset+=chunk_size)
+        {
+            char SendData[32] = {0};
+            size_t bytes_to_copy = std::min(chunk_size, message_size - offset);
+            memcpy(SendData, message + offset, bytes_to_copy);
+
+            ReplySize = sizeof(ICMP_ECHO_REPLY) + sizeof(SendData);
+            ReplyBuffer = (VOID*) malloc(ReplySize);
+            if (ReplyBuffer == nullptr) {
+                throw std::runtime_error("Failed to allocate memory for reply buffer");
             }
-            else {
-                std::cout <<"\tReceived %ld icmp message response\n" << dwRetVal << '\n';
-                std::cout <<"\tInformation from this response:\n" << '\n';
+            dwRetVal = IcmpSendEcho(hIcmpFile,
+                                    ipaddr,
+                                    SendData,
+                                    sizeof(SendData),
+                                    nullptr,
+                                    ReplyBuffer,
+                                    ReplySize,
+                                    1000);
+
+            if (dwRetVal != 0) {
+                auto pEchoReply = (PICMP_ECHO_REPLY)ReplyBuffer;
+                struct in_addr ReplyAddr{};
+                ReplyAddr.S_un.S_addr = pEchoReply->Address;
+                std::cout << "Reply from\n" << inet_ntoa(ReplyAddr) << '\n'
+                          <<"is : " << (char*)pEchoReply->Data << '\n';
+            } else {
+                throw std::runtime_error(&"Failed to send ICMP message "[GetLastError()]);
             }
-            std::cout <<"\t  Received from %s\n" << inet_ntoa( ReplyAddr ) << '\n';
-            std::cout <<"\t  Status = %ld\n" << pEchoReply->Status << '\n';
-            std::cout <<"\t  Roundtrip time = %ld milliseconds\n" << pEchoReply->RoundTripTime <<'\n';
         }
-        else {
-            throw std::runtime_error(&"Failed to send ICMP message " [ GetLastError()]);
-        }
-        free(ReplyBuffer);
-        return ret;
+        return nullptr;
     }
 private:
     std::string ip;
+
     HANDLE hIcmpFile;
     DWORD dwRetVal = 0;
     unsigned long ipaddr;
-    LPVOID ReplyBuffer = NULL;
+    LPVOID ReplyBuffer = nullptr;
     DWORD ReplySize = 0;
-    char SendData[32];
 };
 #endif //SETICMP_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "covert.h"
 #include <iostream>
+#include "stdexcept"
 using namespace std;
 int main()  {
 
     string ip;
     cout << "enter ip address" << '\n';
     cin >> ip;
-
+    try {
+        covert_handler c(ip);
+        char *message = "hello jksdhgkldsjahgajksdhlgjkadshljgldjasghjkljk";
+        c.send_covert_message(message);
+    }catch (const exception& e){
+        cout <<"exception is : "<< e.what() << '\n';
+    }
     return 0;
 }
